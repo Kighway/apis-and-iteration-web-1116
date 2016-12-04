99ba9664fd9922bc332910f9071408eeceae1cf2
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-12-04 16:14:41 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,9 +4,15 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
+  url = 'http://www.swapi.co/api/people/'
+
+  while url
+    page = JSON.parse(RestClient.get(url))
+    found_character = page["results"].find { |character_hash| character_hash["name"].downcase == character }
+    break if found_character
+    url = page["next"]
+  end
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,10 +22,20 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+  found_character["films"].collect { |url| JSON.parse(RestClient.get(url)) }
 end
 
-def parse_character_movies(films_hash)
+
+def parse_character_movies(film_descriptions)
   # some iteration magic and puts out the movies in a nice list
+  roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII"]
+   film_descriptions.each do |description_hash|
+     puts "Star Wars: Episode #{roman_numerals[description_hash["episode_id"]-1]} - #{description_hash["title"]} "
+     puts "Directed by #{description_hash["director"]}"
+     puts "Released on #{description_hash["release_date"]}"
+     puts " "
+   end
 end
 
 def show_character_movies(character)
